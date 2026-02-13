9b606f67925d8c9ab59074d16cc67d3ffddd8a28
Joshua Palma <josua.palma29@gmail.com>
2026-02-13 16:51:33 -0600

    Updated implementation of FireResponder class.

--- PoliceResponder.java
+++ PoliceResponder.java
@@ -1,3 +1,20 @@
-public class PoliceResponder {
-    
+public class PoliceResponder extends Responder implements ResponseTime {
+    private String rank;
+    public PoliceResponder(String rank) {
+        super();
+        this.rank = rank;
+    }
+    public void respondToCrime(Incident incident) {
+        System.out.println("PoliceResponder responding to crime incident at: " + incident.getLocation());
+    }
+    @Override
+    public double calculateResponseTime(Incident incident) {return incident.getSeverityLevel() * 1.2;}
+
+    public String getRank() {return rank;}
+
+    public void setRank(String rank) {this.rank = rank;}
+
+    @Override
+    public String toString() {return "PoliceResponder{" +
+                "rank='" + rank + '\'' + '}';}
 }
