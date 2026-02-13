8fc320b4018b3b579f8cb2ecf13663bbe2254583
Joshua Palma <josua.palma29@gmail.com>
2026-02-13 16:47:13 -0600

    Updated Implementation of FireResponder class.

--- FireResponder.java
+++ FireResponder.java
@@ -1,3 +1,28 @@
-public class FireResponder {
-    
+public class FireResponder extends Responder implements ResponseTime {
+
+    // Attributes
+    private String specialization;
+
+    // Constructor
+    public FireResponder(String specialization) {
+        super();
+        this.specialization = specialization;
+    }
+
+    // Methods
+    public void respondToFire(Incident incident) {
+        System.out.println("FireResponder responding to fire incident at: " + incident.getLocation());
+    }
+
+    @Override
+    public double calculateResponseTime(Incident incident) {return incident.getSeverityLevel() * 1.5;}
+
+    public String getSpecialization() {return specialization;}
+
+    public void setSpecialization(String specialization) {this.specialization = specialization;}
+
+    @Override
+    public String toString() {return "FireResponder{" +
+                "specialization='" + specialization + '\'' +
+                '}';}
 }
